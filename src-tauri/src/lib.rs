94cb6ef80aaff10a0ee09e8fec4bc8fd591bb8e4
jamjotaku <156283662+jamjotaku@users.noreply.github.com>
2026-04-08 14:51:08 +0000

    デバック

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,6 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        // 👇 この行を追加（「tauri-plugin-http」を有効にする）
-        .plugin(tauri_plugin_http::init()) 
-        .setup(|_app| {
-            Ok(())
-        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,5 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // ← 行頭に // を付けてコメントアウト
 
 fn main() {
-    deepsync_widget_lib::run(); 
+    deepsync_widget_lib::run();
 }
